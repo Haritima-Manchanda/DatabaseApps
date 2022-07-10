e95667cf19edd7be761644eb0565829e1826d782
Haritima23 <haritima@udel.edu>
2022-07-10 16:16:19 -0400

    Creating DB tables with python

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -2,4 +2,12 @@ import psycopg2
 
 # connect() actually helps us to connect to the db
 conn = psycopg2.connect(dbname="postgres", user="postgres", password="@Intrtp21!", host="localhost", port="5432")
-print("Connection Successful")
+
+# to execute a query we need to create a cursor
+cur = conn.cursor()
+cur.execute('''CREATE TABLE student(ID Serial, NAME text, ADDRESS text, AGE text);''')
+
+print("Table created")
+# you also need to commit the connection and then close the connection
+conn.commit()
+conn.close()
