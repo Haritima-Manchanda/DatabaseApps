73294f88bdcff39ceff6d23267d8fc88e36b867c
Haritima23 <haritima@udel.edu>
2022-07-10 20:28:23 -0400

    User Submitted Data to DB

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -1,13 +1,28 @@
 import psycopg2
 
-# connect() actually helps us to connect to the db
-conn = psycopg2.connect(dbname="postgres", user="postgres", password="@Intrtp21!", host="localhost", port="5432")
+def create_table():
+    # connect() actually helps us to connect to the db
+    conn = psycopg2.connect(dbname="postgres", user="postgres", password="@Intrtp21!", host="localhost", port="5432")
 
-# to execute a query we need to create a cursor
-cur = conn.cursor()
-cur.execute('''CREATE TABLE student(ID Serial, NAME text, ADDRESS text, AGE text);''')
+    # to execute a query we need to create a cursor
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE student(ID Serial, NAME text, ADDRESS text, AGE text);''')
 
-print("Table created")
-# you also need to commit the connection and then close the connection
-conn.commit()
-conn.close()
+    # you also need to commit the connection and then close the connection
+    conn.commit()
+    conn.close()
+
+def insert_data():
+    name = input("Enter student name: ")
+    address = input("Enter student address: ")
+    age = input("Enter student age: ")
+
+    conn = psycopg2.connect(dbname="postgres", user="postgres", password="@Intrtp21!", host="localhost", port="5432")
+    cur = conn.cursor()
+    query = '''INSERT INTO STUDENT(NAME, ADDRESS, AGE) VALUES (%s, %s, %s)'''
+    cur.execute(query, (name, address, age))
+    conn.commit()
+    conn.close()
+
+#create_table()
+insert_data()
